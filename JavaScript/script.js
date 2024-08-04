5ec918c17d6cac088085989633c7317d600bbce8
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-08-05 03:50:50 +0600

    about section finis

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,3 +3,8 @@ var typed = new Typed(".typing", {
     typeSpeed:70,
     backSpeed:70,
 })
+var typed = new Typed(".typring_about", {
+    strings: ["web designer", "web developer", "MERN Stack developer"],
+    typeSpeed:70,
+    backSpeed:70,
+})
