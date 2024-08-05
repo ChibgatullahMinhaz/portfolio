8d488c9ea2c9dd1e634bee15fae9e9c1199b6219
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-08-06 00:48:03 +0600

    finally adding my contact pages and this page style

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -8,3 +8,17 @@ var typed = new Typed(".typring_about", {
     typeSpeed:70,
     backSpeed:70,
 })
+
+
+// contact message
+document.getElementById('contactForm').addEventListener('submit', (event)=> {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    
+    const mailtoLink = `mailto:chibgatullahminhaz@gmail.com?subject=Contact Form Submission&body=Name: ${name}%0AEmail: ${email}%0AMessage: ${message}`;
+    
+    window.location.href = mailtoLink;
+});
