5a761770b4437916b52089151af8ba975e0cd257
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2024-08-08 01:40:51 +0600

    naber responsive for 1024px done

--- portfolio/script.js
+++ portfolio/script.js
@@ -9,3 +9,19 @@ var typed = new Typed(".typring_about", {
     backSpeed:70,
 })
 
+const openBtn = document.getElementById('open_menu');
+const closeBtn = document.getElementById('close_menu');
+const navMenu = document.getElementById('navMenu');
+
+openBtn.addEventListener('click', () => {
+    openBtn.style.display = 'none';
+    closeBtn.style.display = 'inline-block';
+    navMenu.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    closeBtn.style.display = 'none';
+    openBtn.style.display = 'inline-block';
+    openBtn.style.boxShadow = 'var(--boxShadow)'
+    navMenu.style.display = 'none';
+});
